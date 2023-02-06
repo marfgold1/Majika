87b5006c9e6d38b3bcf6f19be970c0f7cb817713
Amar Fadil <3686-marfgold1@users.noreply.gitlab.informatika.org>
2023-02-06 22:43:17 +0700

    feat: Add splash screen

--- app/src/main/java/com/pap/majika/MenuActivity.kt
+++ app/src/main/java/com/pap/majika/MenuActivity.kt
@@ -2,10 +2,12 @@ package com.pap.majika
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_menu)
     }
 }

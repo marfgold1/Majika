f1e341df558c02f55c3f35e9a3a00aa4c627b0ac
Amar Fadil <3686-marfgold1@users.noreply.gitlab.informatika.org>
2023-02-19 18:30:49 +0700

    feat: update image button style, add call button and more info for branch

--- app/src/main/java/com/pap/majika/pages/branch/BranchItemAdapter.kt
+++ app/src/main/java/com/pap/majika/pages/branch/BranchItemAdapter.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.pap.majika.R
 import com.pap.majika.databinding.FragmentBranchItemBinding
 import com.pap.majika.models.Branch
 
@@ -14,17 +15,34 @@ class BranchItemAdapter(
     class BranchHolder(
         private val binding: FragmentBranchItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(branch: Branch) = with(binding) {
+            val ctx = root.context
             addressText.text = branch.address
             nameText.text = branch.branch_name
-            phoneText.text = branch.phone_number
+            phoneText.text = ctx.getString(
+                R.string.branch_phone_text,
+                branch.phone_number,
+                branch.contact_person,
+            )
+            popularFoodText.text = ctx.getString(
+                R.string.branch_fav_food_text,
+                branch.popular_food,
+            )
             branchMapsBtn.setOnClickListener {
                 val intent = Intent(
                     Intent.ACTION_VIEW,
                     Uri.parse(
-                        "http://maps.google.com/maps?q=loc:${branch.latitude},${branch.longitude} (${branch.branch_name})"
+                        "http://maps.google.com/maps?q=loc:" +
+                                "${branch.latitude},${branch.longitude} " +
+                                "(${branch.branch_name})"
                     ),
                 )
-                val ctx = binding.root.context
+                ctx.startActivity(intent)
+            }
+            branchCallBtn.setOnClickListener {
+                val intent = Intent(
+                    Intent.ACTION_DIAL,
+                    Uri.parse("tel:${branch.phone_number}"),
+                )
                 ctx.startActivity(intent)
             }
         }
